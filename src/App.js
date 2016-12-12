29eaa5acf56a4b8ba37eaa8672b535d9ab83f217
Thomas Kölling <spamdoggi@gmail.com>
2016-12-12 22:36:26 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import RegistrationForm from './RegistrationForm/RegistrationFormContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <RegistrationForm />
+    );
+  }
+}
+
+export default App;
